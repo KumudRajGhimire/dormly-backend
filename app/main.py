0358b164e061f2bad3f2b997096e6841d8bba52b
Kumud Raj Ghimire <kumudraj.cs23@bmsce.ac.in>
2026-07-08 00:39:43 +0530

    removed a slash

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://dormlyshop.vercel.app/"
+        "https://dormlyshop.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
